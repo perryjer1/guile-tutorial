051bc5dc892cee22f9d5dad6016e0f508692c219
Jeremiah Perry <jeremiah.perry@gmail.com>
2015-07-01 06:51:57 -0500

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Guile Tutorial
+
+Pretty much a copy of [this tutorial][http://www.gnu.org/software/guile/docs/guile-tut/tutorial.html].
---
+++ graphics.scm
@@ -0,0 +1,17 @@
+(define (koch-line length depth)
+  (if (zero? depth)
+      (tortoise-move length)
+      (let ((sub-length (/ length 3))
+	    (sub-depth (1- depth)))
+	(for-each (lambda (angle)
+		    (koch-line sub-length sub-depth)
+		    (tortoise-turn angle))
+		  '(60 -120 60 0)))))
+
+(define (snowflake length depth sign)
+  (let iterate ((i 1))
+    (if (<= i 3)
+	(begin
+	  (koch-line length depth)
+	  (tortoise-turn (* sign -120))
+	  (iterate (1+ i))))))
